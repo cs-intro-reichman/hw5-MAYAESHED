3ec0ba1e757e8348a26f5a21841c69d6f651b9b9
Maya Eshed <maya.eshed@post.runi.ac.il>
2024-12-08 20:59:19 +0200

    scrabble testing 2

--- Scrabble.java
+++ Scrabble.java
@@ -52,7 +52,7 @@ public class Scrabble {
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
 		for (int i = 0; i < NUM_OF_WORDS; i++) {
-			if (isEqual(word.toLowerCase(), DICTIONARY[i].toLowerCase())) {
+			if (isEqual(word, DICTIONARY[i])) {
 				return true;
 			}	
 		}
