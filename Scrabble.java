e43a1de4a8936d33b7a144738beba55eb7899eed
Maya Eshed <maya.eshed@post.runi.ac.il>
2024-12-08 22:07:14 +0200

    scrabble testing 10

--- Scrabble.java
+++ Scrabble.java
@@ -112,7 +112,7 @@ public class Scrabble {
 			if (input.equals(".")) {
 				break;
 			} else if (!MyString.subsetOf(input, hand)) {
-				System.out.println( "Invalid word. Try again");
+				System.out.println( "Invalid word. Try again.");
 			} else if (!isWordInDictionary(input)) {
 				System.out.println("No such word in the dictionary. Try again.");
 			} else {
